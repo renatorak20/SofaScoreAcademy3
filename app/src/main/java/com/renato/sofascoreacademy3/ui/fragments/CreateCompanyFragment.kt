884fa81a9ab1fe8ce0bae4c1f192a53e61ceeb25
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2023-03-14 14:23:44 +0100

    Fixed custom_edit_text.xml layout, added more error messages.

--- app/src/main/java/com/renato/sofascoreacademy3/ui/fragments/CreateCompanyFragment.kt
+++ app/src/main/java/com/renato/sofascoreacademy3/ui/fragments/CreateCompanyFragment.kt
@@ -1,6 +1,7 @@
 package com.renato.sofascoreacademy3.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -18,19 +19,7 @@ class CreateCompanyFragment : Fragment() {
     private lateinit var viewModel: CompanyViewModel
 
     private lateinit var constraintLayout: ConstraintLayout
-
-    private lateinit var nameField: CustomEditText
-    private lateinit var addressField: CustomEditText
-    private lateinit var cityField: CustomEditText
-    private lateinit var countryField: CustomEditText
-    private lateinit var founderField: CustomEditText
-    private lateinit var emailField: CustomEditText
-    private lateinit var websiteField: CustomEditText
-    private lateinit var descriptionField: CustomEditText
-    private lateinit var sloganField: CustomEditText
-    private lateinit var spinner: Spinner
     private lateinit var spinnerAdapter: ArrayAdapter<Industry>
-    private lateinit var createButton: Button
 
 
     private lateinit var binding: FragmentCreateCompanyBinding
@@ -43,24 +32,12 @@ class CreateCompanyFragment : Fragment() {
 
         binding = FragmentCreateCompanyBinding.inflate(inflater, container, false)
 
-        nameField = binding.nameField
-        addressField = binding.addressField
-        cityField = binding.cityField
-        countryField = binding.countryField
-        founderField = binding.founderField
-        emailField = binding.emailField
-        websiteField = binding.websiteField
-        descriptionField = binding.descriptionField
-        sloganField = binding.sloganField
-        createButton = binding.button
-        spinner = binding.spinner
-
         constraintLayout = binding.constraintLayout
 
         viewModel = ViewModelProvider(requireActivity())[CompanyViewModel::class.java]
 
         spinnerAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, Industry.values())
-        spinner.adapter = spinnerAdapter
+        binding.spinner.adapter = spinnerAdapter
 
         return binding.root
     }
@@ -68,7 +45,7 @@ class CreateCompanyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        createButton.setOnClickListener {
+        binding.button.setOnClickListener {
             addCompany()
         }
     }
@@ -76,21 +53,21 @@ class CreateCompanyFragment : Fragment() {
     private fun addCompany(){
         if (constraintLayout.areAllFieldsValid()) {
             val company = Company(
-                nameField.getText(),
-                addressField.getText(),
-                cityField.getText(),
-                countryField.getText(),
-                founderField.getText(),
-                emailField.getText(),
-                websiteField.getText(),
-                descriptionField.getText(),
-                sloganField.getText(),
-                spinner.selectedItem as Industry,
+                binding.nameField.getText(),
+                binding.addressField.getText(),
+                binding.cityField.getText(),
+                binding.countryField.getText(),
+                binding.founderField.getText(),
+                binding.emailField.getText(),
+                binding.websiteField.getText(),
+                binding.descriptionField.getText(),
+                binding.foundationField.getText().toInt(),
+                binding.spinner.selectedItem as Industry,
                 getRadioSelectedValue())
 
             viewModel.addCompany(company)
 
-            Toast.makeText(context, "Hurray, You successfully added new company!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, getString(R.string.success_message), Toast.LENGTH_SHORT).show()
 
             constraintLayout.resetFields()
         }
@@ -99,22 +76,19 @@ class CreateCompanyFragment : Fragment() {
     private fun getRadioSelectedValue() : String{
         val type =
             when(binding.radioGroup.radioGroup.checkedRadioButtonId){
-                R.id.company_public -> "Public"
-                else -> "Private"
+                R.id.company_public -> getString(R.string.company_public)
+                else -> getString(R.string.company_private)
             }
         return type
     }
-
     private fun ConstraintLayout.areAllFieldsValid(): Boolean {
         this.children.filterIsInstance<CustomEditText>().forEach {
             if (!it.isValid()) {
-                Toast.makeText(context, "Please enter more than 3 characters in all fields!", Toast.LENGTH_SHORT).show()
                 return false
             }
         }
         return true
     }
-
     private fun ConstraintLayout.resetFields() {
         this.children.filterIsInstance<CustomEditText>().forEach {
             it.clear()
